70e001c8c8bb9371ec646a09cd643f1af421ebd1
chyntiasyf@student.telkomuniversity.ac.id <chyntiasyf@student.telkomuniversity.ac.id>
2020-09-16 12:15:19 +0700

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main (String[] args) {
+	    System.out.println("Nama: Chyntia!");
+		System.out.println("Nim: 6706202125");
+		System.out.println("Kelas: D3IF4402");
+	 }
+}
